379ae15527fd4416b176321c10b6050c1e5d6f94
Your New Username <sahupravin960@gmail.com>
2024-06-28 11:33:20 +0530

    adde landing page

---
+++ src/components/Appbar.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Link, useNavigate } from "react-router-dom";
+
+export function AppBar() {
+   
+  return (
+    <BrowserRouter>
+    <div className="  ">
+      <div className=" flex justify-between ">
+        <div className=" font-bold text-2xl font-manrope  bg-gradient-to-r from-violet-100 to-green-500 inline-block text-transparent bg-clip-text">SyncListener</div>
+        <div>
+            <Link className=" py-2 px-4  dark:bg-white dark:text-black rounded-md shadow-lg font-manrope  " to={"/signin"}>Sign In</Link>
+        </div>
+      </div>
+    </div>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+export function LandingPage() {
+  return (
+    <div className=" flex justify-center font-manrope  min-h-screen  "> 
+      <div className=" flex flex-col justify-center">
+        <div className=" text-7xl font-bold  ">
+          hi there ,<br />
+          welcome to synclistener.
+        </div>
+        <div className=" p-5 text-lg ">
+          a simple voice room where you can listen to music together with sync.
+        </div>
+      </div>
+    </div>
+  );
+}
